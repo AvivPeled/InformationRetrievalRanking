1f120a7180a66de18ffbe19169e736453a0a38e6
yahely <yahel.yechieli@gmail.com>
2015-09-03 19:51:16 +0300

    adding stop words

--- PublicGitRepo/src/Indexer.java
+++ PublicGitRepo/src/Indexer.java
@@ -4,122 +4,90 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.store.RAMDirectory;
+import org.apache.lucene.util.BytesRef;
 import org.apache.lucene.util.Version;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field.Index;
+import org.apache.lucene.document.Field.TermVector;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.index.CorruptIndexException;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.TermFreqVector;
+import org.apache.lucene.search.DocIdSetIterator;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.store.LockObtainFailedException;
 
+
+
 public class Indexer {
 
 	private IndexWriter writer;
+	StopWords stopWords;
+	public Indexer(RAMDirectory idx,String [] docs, StopWords stopWords) throws IOException {
 
-	public Indexer(RAMDirectory idx) throws IOException{
-		 
-		// Construct a RAMDirectory to hold the in-memory representation
-	      // of the index.
-	      
-		StandardAnalyzer analyzer = new StandardAnalyzer(Version.LUCENE_36); 
-		IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_36, analyzer);
-		writer = new IndexWriter(idx, config); 
-	       
+		writer = new IndexWriter(idx, new StandardAnalyzer(Version.LUCENE_36,
+				Collections.emptySet()), true,
+				IndexWriter.MaxFieldLength.UNLIMITED);
+		this.stopWords=stopWords;
+		
+	
 	}
 
-	      /**
-	       * Make a Document object with an un-indexed title field and an
-	       * indexed content field.
-	       */
-	      private static Document createDocument(String title, String content) {
-	         Document doc = new Document();
-	    
-	         // Add the title as an unindexed field...
-	    
-	         doc.add(new Field("title", title, Field.Store.YES, Field.Index.NO));
-	    
-	    
-	         // ...and the content as an indexed field. Note that indexed
-	         // Text fields are constructed using a Reader. Lucene can read
-	         // and index very large chunks of text, without storing the
-	         // entire content verbatim in the index. In this example we
-	         // can just wrap the content string in a StringReader.
-	         doc.add(new Field("content", content, Field.Store.YES, Field.Index.ANALYZED));
-	    
-	         return doc;
-	      }
-	      
-	      
-	private static void addDoc(IndexWriter w, String text, String id) throws IOException {
-		  	Document doc = new Document();
-		  	Document document = new Document();
+	/**
+	 * Make a Document object with an un-indexed title field and an indexed
+	 * content field.
+	 */
+	private static Document createDocument(String title, String content) {
+		Document doc = new Document();
 
-			//index file contents
-			Field contentField = new Field(LuceneConstants.CONTENTS,text,Field.Store.YES,Index.ANALYZED);
-			//index file name
-			Field fileNameField = new Field(LuceneConstants.FILE_NAME,id,Field.Store.YES,Field.Index.NOT_ANALYZED);
-			//index file path
+		// Add the title as an unindexed field...
+
+		doc.add(new Field("title", title, Field.Store.YES, Field.Index.NO));
+
+		// ...and the content as an indexed field. Note that indexed
+		// Text fields are constructed using a Reader. Lucene can read
+		// and index very large chunks of text, without storing the
+		// entire content verbatim in the index. In this example we
+		// can just wrap the content string in a StringReader.
+		doc.add(new Field("content", content, Field.Store.YES,
+				Field.Index.ANALYZED, Field.TermVector.YES));
+
+		return doc;
+	}
+
+	private void addDocs(String[] docs) throws Exception {
+		for (int i = 0; i < docs.length; i++) {
 			
-			document.add(contentField);
-			document.add(fileNameField);
-	
-			w.addDocument(doc);
+			writer.addDocument(createDocument(Integer.toString(i), docs[i]));
+		
 		}
-	
-	@SuppressWarnings("deprecation")
-	private Document getDocument(File file) throws IOException
-	{
-		Document document = new Document();
 
-		//index file contents
-		Field contentField = new Field(LuceneConstants.CONTENTS,new FileReader(file));
-		//index file name
-		Field fileNameField = new Field(LuceneConstants.FILE_NAME,file.getName(),Field.Store.YES,Field.Index.NOT_ANALYZED);
-		//index file path
-		Field filePathField = new Field(LuceneConstants.FILE_PATH,file.getCanonicalPath(),Field.Store.YES,Field.Index.NOT_ANALYZED);
-
-		document.add(contentField);
-		document.add(fileNameField);
-		document.add(filePathField);
-
-		return document;
 	}
 
-   private void indexFile(File file) throws IOException{
-	      System.out.println("Indexing "+file.getCanonicalPath());
-	      Document document = getDocument(file);
-	      writer.addDocument(document);
-	   }
-
-   
-   private void addDocs(String [] docs) throws CorruptIndexException, IOException
-   {
-	   for(int i=0;i<docs.length;i++)
-	   {
-		   writer.addDocument(createDocument(Integer.toString(i),docs[i]));
-		   
-	   }
-
-   }
-	public void createIndex(String [] docs) 
-      throws IOException{
+	public void createIndex(String[] docs) throws Exception {
 		addDocs(docs);
-     
-	   }
-     
-	
 
-	public void close() throws CorruptIndexException, IOException{
-    writer.close();
 	}
+
+	public void close() throws CorruptIndexException, IOException {
+		writer.close();
+	}
+
 }
--- PublicGitRepo/src/LuceneTester.java
+++ PublicGitRepo/src/LuceneTester.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -19,50 +18,58 @@ import readingInputFiles.ReadingParameterFile;
 import readingInputFiles.ReadingQueriesFile;
 
 public class LuceneTester {
-	
-   static String parameterFilePath="parameterFile.txt";
-   static String  dataDir= "Data";
-   String  indexDir= "Index";
-   Indexer indexer;
-   static RAMDirectory idx;
-   public static void main(String[] args)  {
-      LuceneTester tester;
-      try {
-    	  
-    	  idx = new RAMDirectory();
-    	  CreateInputFiles createInputFiles=new CreateInputFiles(parameterFilePath);
-  		  createInputFiles.Create(dataDir);
-  		  String [] docs=createInputFiles.getDocs();
-          tester = new LuceneTester();
-          tester.createIndex(docs);
-         
-          ReadingParameterFile parameterFile = new ReadingParameterFile(parameterFilePath);
-  		  parameterFile.readFile();
-          ReadingQueriesFile queriesFile = new ReadingQueriesFile(parameterFile.getQueryFileName());
-  		  queriesFile.readFile();
-  		
-  		  String [] dict= queriesFile.getDictonaryNumberQueryToQuery();
-  		  System.out.println(dict[3]);
-         
-  		  tester.search(dict[3]);
-         StopWords stopWords=new StopWords();
-         stopWords.computeTopTermQuery(idx);
-         List<String> topTerms=stopWords.getTopTerms();
-         Map<String,Integer> frequencyMap= stopWords.getFrequencyMap();
-      } catch (Exception e) {
-         e.printStackTrace();
-      } 
-   }
-    
-   private void createIndex(String [] docs) throws IOException{
-      indexer = new Indexer(idx);
-      indexer.createIndex(docs);
-      indexer.close();      
-   }
-   
-   private void search(String searchQuery) throws IOException, ParseException{
-	      Searcher searcher = new Searcher(idx);
-	     searcher.search(searchQuery);
-	      searcher.close();
-	   }
+
+	static String parameterFilePath = "parameterFile.txt";
+	static String dataDir = "Data";
+	String indexDir = "Index";
+	Indexer indexer;
+	static RAMDirectory idx;
+
+	public static void main(String[] args) {
+		LuceneTester tester;
+		try {
+			// HighFreqTerms f;
+			StopWords stopWords=new StopWords();
+			idx = new RAMDirectory();
+			CreateInputFiles createInputFiles = new CreateInputFiles(
+					parameterFilePath);
+			
+			createInputFiles.Create(dataDir);
+			String[] docs = createInputFiles.getDocs();
+			tester = new LuceneTester();
+			
+			tester.createIndex(docs,stopWords);
+			stopWords.calculateFrequency(docs, idx);
+			ReadingParameterFile parameterFile = new ReadingParameterFile(
+					parameterFilePath);
+			parameterFile.readFile();
+			ReadingQueriesFile queriesFile = new ReadingQueriesFile(
+					parameterFile.getQueryFileName());
+			queriesFile.readFile();
+
+			String[] dict = queriesFile.getDictonaryNumberQueryToQuery();
+			System.out.println(dict[3]);
+			
+			tester.search(dict[3],docs,stopWords);
+			
+			
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void createIndex(String[] docs, StopWords stopWords) throws Exception {
+		indexer = new Indexer(idx,docs,stopWords);
+		indexer.createIndex(docs);
+		indexer.close();
+		
+	}
+
+	private void search(String searchQuery, String [] docs, StopWords stopWords) throws Exception {
+		Searcher searcher = new Searcher(idx, docs,stopWords);
+		searcher.search(searchQuery);
+		searcher.close();
+
+	}
 }
